f55000808c0853ad30c2277397e63fa63af17874
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2020-03-16 11:59:20 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,10 @@
+# Aligned with opening delimiter.
+grow = function_name(variable_one, variable_two,
+                     variable_three, variable_four)
+# First line contains no argument. Second line onwards
+# more indentation included to distinguish this from 
+# the rest.
+def function_name(
+        variable_one, variable_two, variable_three,
+        variable_four):
+    print(variable_one)
